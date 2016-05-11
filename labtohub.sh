4ef789ade27d023f260dbc4c2ebafda089dda32b
Tiago <tiagoadias@gmail.com>
2016-05-11 21:08:55 +0100

    added quiet mode

--- labtohub.sh
+++ labtohub.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+# defaults
+GIT_COMMAND="git"
+DELETE_WORKSPACE="ON"
+
+# import config
 source config.sh
 
 # temporary files directory
@@ -7,12 +13,19 @@ cd $TMP_DIR
 
 # log output
 log() {
-  echo "> $@"
+  if [[ "$QUIET" != "ON" ]]; then
+    echo "$@"
+  fi
+}
+
+# error output
+error() {
+  echo "$@" >&2
 }
 
 # suppress output
 quiet() {
-  $@ 2>&1 >/dev/null
+  $@ 2>/dev/null >/dev/null
 }
 
 # api call
@@ -23,7 +36,7 @@ api() {
     local data="-d ${@:3}"
   fi
 
-  curl --silent -u "$HUBUSER:$HUBPASS" -X $1 https://api.github.com/$2 "$data"
+  curl --silent -u "$HUBUSER:$HUBPASS" -X $1 https://api.github.com$2 "$data"
 }
 
 # clean up string
@@ -36,10 +49,22 @@ clean() {
   echo $escaped
 }
 
+# dependency "management"
 depend() {
   if ! hash $1 2>/dev/null; then
-    echo "the command $1 doesn't exist"
+    error "the command $1 doesn't exist"
     exit 1
+  fi
+}
+
+# run git command
+git_run() {
+  export GIT_ASKPASS=echo
+
+  if [[ "$QUIET" == "ON" && "$1" != "remote" ]]; then
+    git $@ --quiet
+  else
+    git $@
   fi
 }
 
@@ -53,43 +78,53 @@ for repo in ${LABREPOS[@]}; do
   hubrepo=git@github.com:$HUBUSER/$repo.git
   labrepo=https://gitlab.com/$LABUSER/$repo.git
 
-  # fetch the old description from the api
-  old_description=$(api GET repos/$HUBUSER/$repo | jq -r .description | clean)
+  # get the repo info from the api
+  repo_json=$(api GET /repos/$HUBUSER/$repo)
 
   # initialize json with repo name
   json="{}"
   json=$(echo $json | jq ".name = \"$repo\"")
 
-  # prefix description with prefix if it doesn't contain "mirror"
-  if [[ "$old_description" != *"$MIRRORPREFIX"* ]]; then
-    new_description=$MIRRORPREFIX$old_description
-
-    json=$(echo $json | jq ".description = \"$new_description\"")
-  fi
-
   # replace homepage with original repo url
   homepage=https://gitlab.com/$LABUSER/$repo
   json=$(echo $json | jq ".homepage = \"$homepage\"")
 
-  # commit the repo metadata
-  quiet api PATCH repos/$HUBUSER/$repo "$(echo $json)"
+  # create the repo if it doesn't exist
+  if [[ $(echo $repo_json | jq -r .message) == "Not Found" ]]; then
+    json=$(echo $json | jq ".description = \"$MIRRORPREFIX\"")
+
+    log "creating repo $repo on github"
+    quiet api POST /user/repos "$(echo $json)"
+  # if it exists then change the description and stuff
+  else
+    old_description=$(echo $repo_json | jq -r .description | clean)
+
+    # prefix description with prefix if it doesn't contain "mirror"
+    if [[ "$old_description" != *"$MIRRORPREFIX"* ]]; then
+      new_description=$MIRRORPREFIX$old_description
+
+      json=$(echo $json | jq ".description = \"$new_description\"")
+    fi
+
+    quiet api PATCH /repos/$HUBUSER/$repo "$(echo $json)"
+  fi
 
   # only clone if the mirror directory doesn't exist already
   if [ -d $repo.git ]; then
     quiet pushd $repo.git
     log "fetching repository $labrepo"
-    git fetch -q --tags
+    git_run fetch -q --tags
   else
     log "cloning repository $labrepo"
-    git clone --mirror $labrepo
+    git_run clone --mirror $labrepo
     quiet pushd $repo.git
 
     log "added remote github: $hubrepo"
-    git remote add github $hubrepo
+    git_run remote add github $hubrepo
   fi
 
   log "pushing to repository $hubrepo"
-  git push --mirror $hubrepo
+  git_run push --mirror $hubrepo
   quiet popd
 
   if [[ "$DELETE_WORKSPACE" == "ON" ]]; then
